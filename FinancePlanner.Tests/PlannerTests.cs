5ad80e8ce672f995f25ec683264da9177062c8c8
Ivan <ta31.nyzchnyk.ivan@gmail.com>
2025-11-01 07:59:32 +0200

    feat(tests): add instantiation test and minimal FinancePlanner class

---
+++ FinancePlanner.Tests/PlannerTests.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+using FinancePlanner;
+
+namespace FinancePlanner.Tests
+{
+    public class PlannerTests
+    {
+        [Fact]
+        public void CanCreatePlannerInstance()
+        {
+            var planner = new FinPlanner();
+            Assert.NotNull(planner);
+        }
+    }
+}
